5bb098d0eb7f48f741b19b9f7a6d9a6ff19b89e1
Piven Vadim <vp29101998q@gmail.com>
2021-06-15 01:33:16 +0900

    update font

--- Main/src/CalculatorFrame.java
+++ Main/src/CalculatorFrame.java
@@ -9,6 +9,8 @@ import Enum.ActionsOnOne;
 
 public class CalculatorFrame extends JFrame {
 
+    private static final Font FONT = new Font("TimesRoman", Font.BOLD, 30);
+
     private JTextField inputArea;
 
     public CalculatorFrame() {
@@ -16,10 +18,13 @@ public class CalculatorFrame extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         setBounds(100, 100, 600, 500);
+        getRootPane().setBorder(BorderFactory.createEmptyBorder(3, 5, 3, 5));
 
         setJMenuBar(new MenuBar());
 
-        setLayout(new BorderLayout());
+        BorderLayout borderLayout = new BorderLayout();
+        borderLayout.setVgap(3);
+        setLayout(borderLayout);
 
         add(createTopPanel(), BorderLayout.NORTH);
         add(createMeinPanel(), BorderLayout.CENTER);
@@ -30,7 +35,6 @@ public class CalculatorFrame extends JFrame {
     private JPanel createTopPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
-
         inputArea = new JTextField();
         inputArea.setEditable(false);
 
@@ -69,19 +73,23 @@ public class CalculatorFrame extends JFrame {
         for (int i = 1; i < 10; i++) {
             JButton btn = new JButton(String.valueOf(i));
             btn.addActionListener(setNameActionListener);
+            btn.setFont(FONT);
             panel.add(btn);
         }
 
         JButton clear = new JButton("C");
         clear.addActionListener(e -> inputArea.setText(""));
+        clear.setFont(FONT);
         panel.add(clear);
 
         JButton btn0 = new JButton("0");
         btn0.addActionListener(setNameActionListener);
+        btn0.setFont(FONT);
         panel.add(btn0);
 
         JButton calc = new JButton("=");
         calc.addActionListener(new CalcButtonActionListener(inputArea));
+        calc.setFont(FONT);
         panel.add(calc);
 
         return panel;
@@ -97,6 +105,7 @@ public class CalculatorFrame extends JFrame {
 
         JButton btnPoint = new JButton(".");
         btnPoint.addActionListener(new SetPointActionListener(inputArea));
+        btnPoint.setFont(FONT);
         panel.add(btnPoint);
         ActionSetNameOperatorOnTwoActionListener actionListenerTwo = new ActionSetNameOperatorOnTwoActionListener(inputArea);
 
@@ -104,6 +113,7 @@ public class CalculatorFrame extends JFrame {
         for (int i = 0; i < ActionsOnTwo.length; i++) {
             JButton btn = new JButton(ActionsOnTwo.getTitle(i));
             btn.addActionListener(actionListenerTwo);
+            btn.setFont(FONT);
             panel.add(btn);
         }
 
@@ -111,6 +121,7 @@ public class CalculatorFrame extends JFrame {
         for (int i = 0; i < ActionsOnOne.length; i++) {
             JButton btn = new JButton(ActionsOnOne.getTitle(i));
             btn.addActionListener(actionListenerOne);
+            btn.setFont(FONT);
             panel.add(btn);
         }
 
