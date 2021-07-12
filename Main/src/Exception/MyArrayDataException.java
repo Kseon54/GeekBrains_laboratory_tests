270c738be1df4fd7e5e80df05b4995e3cbc906e4
Piven Vadim <vp29101998q@gmail.com>
2021-07-12 20:00:30 +0900

    done

---
+++ Main/src/Exception/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class MyArrayDataException extends IllegalArgumentException {
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+
+    public MyArrayDataException() {
+    }
+}
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
+import Exception.MyArraySizeException;
+import Exception.MyArrayDataException;
 
+public class Main {
+
+    public static final int N = 4;
+
+    public static void main(String[] args) {
+        String[][] strings = {
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+        };
+
+        try {
+            System.out.println(arrayStringToArrayInt(strings));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int arrayStringToArrayInt(String[][] strings) {
+        if (strings.length != N) throw new MyArraySizeException(String.format("Ошибка. Массив не %dх%d.", N, N));
+        for (String[] string : strings)
+            if (string.length != N) throw new MyArraySizeException(String.format("Ошибка. Массив не %dх%d.", N, N));
+
+        int a = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                try {
+                    a += Integer.parseInt(strings[i][j].trim());
+                } catch (Exception e) {
+                    throw new MyArrayDataException(String.format("Ошибка. Элемент [%d][%d] не int.", i + 1, j + 1));
+                }
+            }
+        }
+        return a;
     }
 }
